bbdc703deaaf65d86b434135beec7cf6645f43e3
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-06 15:08:48 +0100

    Add profiles app and core model.

--- CrepChat/settings.py
+++ CrepChat/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get["SECRET_KEY"]
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "cloudinary_storage",
     "django.contrib.staticfiles",
     "cloudinary",
+    "profiles",
 ]
 
 MIDDLEWARE = [
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# CrepChat-DRF-API
+# Crep Chat Backend
+
+Developer: Tom Ainsworth
+
+This repository contains the API for the full stack application Crep Chat - a content site for trainer enthusiasts to chat about their favourite pairs.
+It's built using Django and Django Rest Framework.
+
+Link to the frontend repository [Crep Chat frontend](https://github.com/Tom-Ainsworth/crepchat-frontend)
