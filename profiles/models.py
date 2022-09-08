a73f9c4b14ce23384cb7cbb376f2a8df88784868
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-08 19:42:28 +0100

    Fix model type in owner field. Add meta ordering and str method for admin panel

--- profiles/models.py
+++ profiles/models.py
@@ -1,10 +1,9 @@
-from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    owner = models.OneToOneField(User, on_delete=CASCADE)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
@@ -14,3 +13,9 @@ class Profile(models.Model):
         default="../default_profile_kkzjl7.jpg",
         blank=True,
     )
+
+    class Meta:
+        ordering = ['-created_at']
+        
+    def __str__(self):
+        return f"{self.owner}'s profile"
