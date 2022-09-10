4187e2ca6b24523ef4ed006d080e1f370cf63f28
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-10 16:22:15 +0100

    Add put method to ProfileDetails to update existing profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,9 @@
 # External
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
+
 
 # Internal
 from .models import Profile
@@ -18,14 +20,28 @@ class ProfileList(APIView):
     
 
 class ProfileDetail(APIView):
+    '''View to retrieve and update a profile'''
+    
+    serializer_class = ProfileSerializer
+    
     def get_object(self, pk):
         try:
-            profile=Profile.objects.get(pk=pk)
+            profile = Profile.objects.get(pk=pk)
             return profile
         except Profile.DoesNotExist:
             raise Http404
         
     def get(self, request, pk):
         profile = self.get_object(pk)
-        serializer=ProfileSerializer(profile)
+        serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+    
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
