42da7a2382f04100ffd5ab7ff559363dff03f35e
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-10 21:10:38 +0100

    Add postlistview tests for listing posts, and logged in user can create a post

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from urllib import response
+from profiles.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='steve', password='password')
+        
+    def test_can_list_posts(self):
+        steve = User.objects.get(username='steve')
+        Post.objects.create(owner=steve, title='title')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
+        
+    def test_logged_in_user_can_create_post(self):
+        self.client.login(username='steve', password='password')
+        response = self.client.post('/posts/', {'title': 'title'})
+        count = Post.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
