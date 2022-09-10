346991c83b656e46934316ac50037f9787a9d8b0
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-10 16:10:35 +0100

    Add serializer to profiles app to display profiles in rest framework

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,16 @@
+# External
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+# Internal
 from .models import Profile
+from .serializers import ProfileSerializer
+
 
 class ProfileList(APIView):
     '''View to display a list of all profiles'''
     
     def get(self, request):
         profiles = Profile.objects.all()
-        return Response(profiles)
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
