6612ac569b860089e26b95f2f8e5a2d343aa2a41
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-10 20:38:55 +0100

    Add posts to installed apps. Add PostDetail view and migrate database changes

--- posts/views.py
+++ posts/views.py
@@ -2,10 +2,13 @@
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.http import Http404
+
 
 # Internal
 from .models import Post
 from .serializers import PostSerializer
+from CrepChat.permissions import IsOwnerOrReadOnly
 
 class PostList(APIView):
     '''List view for all posts'''
@@ -26,4 +29,33 @@ class PostList(APIView):
             serializer.save(owner=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class PostDetail(APIView):
+    '''Detail view to retrieve and update a post'''
+    
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            return post
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, context={"request": request})
+        self.check_object_permissions(self.request, post)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, context={"request": request})
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
