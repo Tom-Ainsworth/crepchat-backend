264f8d74ec422330b8af99dab80d652f8191dc2e
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-12 15:44:12 +0100

    Add category field to post model

--- posts/models.py
+++ posts/models.py
@@ -8,6 +8,12 @@ class Post(models.Model):
     Default image set so that we can always reference image.url.
     """
 
+    CATEGORY_CHOICES = [
+        ("ADIDAS", "Adidas"),
+        ("NEW_BALANCE", "New Balance"),
+        ("NIKE", "Nike"),
+    ]
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -16,6 +22,7 @@ class Post(models.Model):
     image = models.ImageField(
         upload_to="images/", default="../default_post_q8rncs.jpg", blank=True
     )
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
 
     class Meta:
         ordering = ["-created_at"]
