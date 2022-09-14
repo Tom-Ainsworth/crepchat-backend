c741e336343830c158674d009c88f940a8274c95
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-14 15:42:57 +0100

    Add LikeList and LikeDetail views. Add urls in likes and CrepChat folders

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+# Internal
+from CrepChat.permissions import IsOwnerOrReadOnly
+from .models import Like, Dislike
+from .serializers import LikeSerializer, DislikeSerializer
+
+
+class LikeList(generics.ListCreateAPIView):
+    """Generic list view for comments"""
+
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """Generic view to retrive and delete likes. No need to update them"""
+
+    serializer_class = LikeSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Like.objects.all()
