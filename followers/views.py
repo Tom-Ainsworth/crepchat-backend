a55c3d5690b43d1f04a21f0a7aee8edef1e8274f
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-14 17:19:22 +0100

    Add FollowerList and FollowerDetail to add, view and remove followers

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+# External
+from rest_framework import generics, permissions
 
-# Create your views here.
+# Internal
+from CrepChat.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
+
+
+class FollowerList(generics.ListCreateAPIView):
+    """View for displaying and creating a new follower"""
+
+    serializer_class = FollowerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    serializer_class = FollowerSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Follower.objects.all()
