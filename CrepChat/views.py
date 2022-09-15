13ec58e2fed4a8f77c4612ec9fd0db34b88a19b2
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-15 19:18:48 +0100

    Add root_route view to display a message when opening the api root endpoint

---
+++ CrepChat/views.py
@@ -0,0 +1,8 @@
+# External
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view()
+def root_route(request):
+    return Response({"message": "Welcome to the Crep Chat backend API"})
