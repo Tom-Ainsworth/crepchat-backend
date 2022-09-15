e4f791e63031e7121379c2460f9789c5d26a93d5
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-15 12:33:01 +0100

    Add get_likes_id and get_dislikes_id methods to PostSerializer to retrieve like/dislike data for each post

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like, Dislike
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,6 +8,8 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source="owner.profile.id")
     profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
+    like_id = serializers.SerializerMethodField()
+    dislike_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -21,6 +24,20 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context["request"]
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context["request"].user
+        if user.is_authenticated:
+            like = Like.objects.filter(owner=user, post=obj).first()
+            return like.id if like else None
+        return None
+
+    def get_dislike_id(self, obj):
+        user = self.context["request"].user
+        if user.is_authenticated:
+            dislike = Dislike.objects.filter(owner=user, post=obj).first()
+            return dislike.id if dislike else None
+        return None
+
     class Meta:
         model = Post
         fields = [
@@ -34,4 +51,6 @@ class PostSerializer(serializers.ModelSerializer):
             "caption",
             "image",
             "category",
+            "like_id",
+            "dislike_id",
         ]
