892f54038924d0182892945a77922a7cdc2e4153
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-15 16:23:26 +0100

    Amend PostList and PostDetail querysets to filter the total posts, followers, and following counts for a given profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 # External
-from rest_framework import generics
+from rest_framework import generics, filters
+from django.db.models import Count
 
 # Internal
 from .models import Profile
@@ -11,7 +12,19 @@ class ProfileList(generics.ListAPIView):
     """Generic list view for all profiles. Profile creation is handled by django signals from the user instance, so no need for a create view"""
 
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count("owner__post", distinct=True),
+        followers_count=Count("owner__followed", distinct=True),
+        following_count=Count("owner__following", distinct=True),
+    ).order_by("-created_at")
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = [
+        "posts_count",
+        "followers_count",
+        "following_count",
+        "owner__following__created_at",
+        "owner__followed__created_at",
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -19,4 +32,9 @@ class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
 
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+
+    queryset = Profile.objects.annotate(
+        posts_count=Count("owner__post", distinct=True),
+        followers_count=Count("owner__followed", distinct=True),
+        following_count=Count("owner__following", distinct=True),
+    ).order_by("-created_at")
