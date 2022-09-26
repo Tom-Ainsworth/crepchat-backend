c329d68763a425b8d6566ac0c1b8ba642ced15a5
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-26 15:58:17 +0100

    Change caption field to a TextField to allow more user input

--- posts/models.py
+++ posts/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    caption = models.CharField(max_length=255)
+    caption = models.TextField(blank=True)
     image = models.ImageField(
         upload_to="images/", default="../default_post_q8rncs.jpg", blank=True
     )
