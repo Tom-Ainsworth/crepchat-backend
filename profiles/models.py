e62c90f11551adf6d5082981cf045e1200ee6823
Tom Ainsworth <tomainsworth94@gmail.com>
2023-04-30 18:51:33 +0100

    Alter image field on profile model

--- profiles/models.py
+++ profiles/models.py
@@ -20,7 +20,7 @@ class Profile(models.Model):
     content = models.TextField(blank=True)
     image = models.ImageField(
         upload_to="images/",
-        default="default_profile_zcy3qc",
+        default="default_profile_kkzjl7_tv9li6.jpg",
         blank=True,
     )
 
